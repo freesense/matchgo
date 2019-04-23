80e25f1e06c47c913e6e409022af997c47cc29f6
freesense <freesense@126.com>
2019-04-23 10:03:55 +0800

    准备合并到bidding_matcher项目中作为子模块存在

--- build.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-../bidding_matcher/encryptlua.py
-../bidding_matcher/buildver.py
-cp ../bidding_matcher/matcher.ini ./
-./goinstall public
-./goinstall matchgo
-./goinstall maintain
